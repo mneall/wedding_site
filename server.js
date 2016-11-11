4f3f48450735c5501fdb8643768d322e2c352d4f
Matthew Neall <neallmr@gmail.com>
2016-11-10 16:14:27 -0800

    add wedding div

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ var express = require('express'),
 	path = require('path');
 
 app.use(express.static(path.join(__dirname, 'client')));
+app.set('views', __dirname + '/client/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// ROUTES
 
 app.get('/', function(req, res){
-	res.render('index');
+	res.render('index.html');
 });
+
+app.get('/bridalparty', function(req, res){
+	res.render('bridalparty.html');
+});
+
+// RUN SERVER
 
 app.listen(8000, function(){
 	console.log('server is running');
